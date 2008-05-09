c019a0bccbf5f4109018414ed2ff8e816e889061
Sven Nierlein <sven@nierlein.de>
2008-05-09 20:47:39 +0000

    fixed sample config

--- www/index.php
+++ www/index.php
@@ -25,11 +25,6 @@
 error_reporting(2047);
 
 ### INCLUDES ###
-include("config.php");
-if($config["accControl"] == 1 AND isset($_SERVER["REMOTE_ADDR"]) AND !in_array($_SERVER["REMOTE_ADDR"], $config["allowedIPs"])) {
-    die($_SERVER["REMOTE_ADDR"]." ist nicht zugelassen");
-}
-
 include("include/common.php");
 include("include/Template.php");
 include("include/Action.php");
@@ -42,101 +37,116 @@ include("include/Action.php");
 
 function action_default()
 {
-    global $config;
-
     $failed = 0;
 
     print "<html><head><title>WebMP3 configuration check...</title></head><body>\n";
     print "doing some sanity checks...<br>";
 
-    # check search path
-    if(!isset($config["searchPath"])) {
-        print "ERROR: music directory option missing, please set searchPath in the config.php<br>\n";
-        $failed = 1;
-    }
-    elseif(!is_dir($config["searchPath"])) {
-        print "ERROR: ".$config["searchPath"]." is not a valid directory<br>\n";
-        $failed = 1;
-    }
-
-    # check var path
-    if(!isset($config["plDir"])) {
-        print "ERROR: playlists directory option missing, please set plDir in the config.php<br>\n";
-        $failed = 1;
-    }
-    elseif(!file_exists($config["plDir"])) {
-        print "INFO: trying to create ".$config["plDir"]."<br>\n";
-        mkdir($config['plDir']);
-        $failed = 1;
-    }
-    elseif(!is_dir($config["plDir"])) {
-        print "INFO: playlist directory ".$config["plDir"]." is not a valid directory<br>\n";
-        $failed = 1;
-    }
-
-    # check binarys
-    if(!isset($config["ext"]) or !is_array($config["ext"])) {
-        print 'ERROR: no extensions configured, $config["ext"] is not a vaild array<br>\n';
-        $failed = 1;
-    } else {
-        foreach($config["ext"] as $name => $ext) {
-            if(!isset($ext['binary'])) {
-                print "ERROR: Extension ".$name." has no binary configured<br>\n";
-                $failed = 1;
-            }
-            elseif(!file_exists($ext['binary'])) {
-                print "ERROR: ".$ext['binary']." does not exist<br>\n";
-                $failed = 1;
-            }
-            elseif(!is_executable($ext['binary'])) {
-                print "ERROR: ".$ext['binary']." is not executable<br>\n";
-                $failed = 1;
-            }
+    if(!file_exists("config.php")) {
+        if(file_exists("config.php.sample")) {
+          print "INFO: trying to copy config.php.sample to config.php<br>\n";
+          copy("config.php.sample", "config.php");
         }
     }
-
-    # check the php binary
-    if(!isset($config["cliPHPbinary"])) {
-        print "ERROR: php binary option missing, please set cliPHPbinary in the config.php<br>\n";
-        $failed = 1;
-    }
-    elseif(!file_exists($config["cliPHPbinary"])) {
-        print "ERROR: ".$config["cliPHPbinary"]." does not exist<br>\n";
-        $failed = 1;
-    }
-    elseif(!is_executable($config["cliPHPbinary"])) {
-        print "ERROR: ".$config["cliPHPbinary"]." is not executable<br>\n";
+    if(!file_exists("config.php")) {
+        print "ERROR: no config.php found<br>\n";
         $failed = 1;
     }
 
-    # check the stream binary
-    if(!isset($config["streamBin"])) {
-        print "ERROR: stream binary option missing, please set streamBin in the config.php<br>\n";
-        $failed = 1;
-    }
-    elseif(!file_exists($config["streamBin"])) {
-        print "ERROR: ".$config["streamBin"]." does not exist<br>\n";
-        $failed = 1;
-    }
-    elseif(!is_executable($config["streamBin"])) {
-        print "ERROR: ".$config["streamBin"]." is not executable<br>\n";
-        $failed = 1;
-    }
+    if(file_exists("config.php")) {
+        include("config.php");
+        global $config;
 
-    if(!isset($config["aumixBin"])) {
-        print "ERROR: aumix binary option missing, please set aumixBin in the config.php<br>\n";
-        $failed = 1;
-    }
-    elseif(!is_executable($config["aumixBin"])) {
-        print "ERROR: ".$config["aumixBin"]." is not executable<br>\n";
-        $failed = 1;
-    } else {
-        $vol = getVolume();
-        if(!is_numeric($vol)) {
-            print "ERROR: failed to get volume<br>\n";
-            print "ERROR: please check your aumixBin: ".$config["aumixBin"]."<br>\n";
-            print "ERROR: also check if your webserver user is in group audio<br>\n";
+        # check search path
+        if(!isset($config["searchPath"])) {
+            print "ERROR: music directory option missing, please set searchPath in the config.php<br>\n";
             $failed = 1;
+        }
+        elseif(!is_dir($config["searchPath"])) {
+            print "ERROR: ".$config["searchPath"]." is not a valid directory<br>\n";
+            $failed = 1;
+        }
+
+        # check var path
+        if(!isset($config["plDir"])) {
+            print "ERROR: playlists directory option missing, please set plDir in the config.php<br>\n";
+            $failed = 1;
+        }
+        elseif(!file_exists($config["plDir"])) {
+            print "INFO: trying to create ".$config["plDir"]."<br>\n";
+            mkdir($config['plDir']);
+            $failed = 1;
+        }
+        elseif(!is_dir($config["plDir"])) {
+            print "INFO: playlist directory ".$config["plDir"]." is not a valid directory<br>\n";
+            $failed = 1;
+        }
+
+        # check binarys
+        if(!isset($config["ext"]) or !is_array($config["ext"])) {
+            print 'ERROR: no extensions configured, $config["ext"] is not a vaild array<br>';
+            print "\n";
+            $failed = 1;
+        } else {
+            foreach($config["ext"] as $name => $ext) {
+                if(!isset($ext['binary'])) {
+                    print "ERROR: Extension ".$name." has no binary configured<br>\n";
+                    $failed = 1;
+                }
+                elseif(!file_exists($ext['binary'])) {
+                    print "ERROR: ".$ext['binary']." does not exist<br>\n";
+                    $failed = 1;
+                }
+                elseif(!is_executable($ext['binary'])) {
+                    print "ERROR: ".$ext['binary']." is not executable<br>\n";
+                    $failed = 1;
+                }
+            }
+        }
+
+        # check the php binary
+        if(!isset($config["cliPHPbinary"])) {
+            print "ERROR: php binary option missing, please set cliPHPbinary in the config.php<br>\n";
+            $failed = 1;
+        }
+        elseif(!file_exists($config["cliPHPbinary"])) {
+            print "ERROR: ".$config["cliPHPbinary"]." does not exist<br>\n";
+            $failed = 1;
+        }
+        elseif(!is_executable($config["cliPHPbinary"])) {
+            print "ERROR: ".$config["cliPHPbinary"]." is not executable<br>\n";
+            $failed = 1;
+        }
+
+        # check the stream binary
+        if(!isset($config["streamBin"])) {
+            print "ERROR: stream binary option missing, please set streamBin in the config.php<br>\n";
+            $failed = 1;
+        }
+        elseif(!file_exists($config["streamBin"])) {
+            print "ERROR: ".$config["streamBin"]." does not exist<br>\n";
+            $failed = 1;
+        }
+        elseif(!is_executable($config["streamBin"])) {
+            print "ERROR: ".$config["streamBin"]." is not executable<br>\n";
+            $failed = 1;
+        }
+
+        if(!isset($config["aumixBin"])) {
+            print "ERROR: aumix binary option missing, please set aumixBin in the config.php<br>\n";
+            $failed = 1;
+        }
+        elseif(!is_executable($config["aumixBin"])) {
+            print "ERROR: ".$config["aumixBin"]." is not executable<br>\n";
+            $failed = 1;
+        } else {
+            $vol = getVolume();
+            if(!is_numeric($vol)) {
+                print "ERROR: failed to get volume<br>\n";
+                print "ERROR: please check your aumixBin: ".$config["aumixBin"]."<br>\n";
+                print "ERROR: also check if your webserver user is in group audio<br>\n";
+                $failed = 1;
+            }
         }
     }
 
@@ -158,7 +168,6 @@ function action_default()
     }
 
     print "</body></html>\n";
-    //redirect("webmp3.php");
 }
 
 #################################################################
