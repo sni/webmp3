fc140457ce1baa7d87781e74fd11120db061a1d6
Sven Nierlein <sven@nierlein.de>
2008-12-23 12:33:34 +0000

    new volume.sh for osx,oss and alsa

--- www/volume.sh
+++ www/volume.sh
@@ -1,57 +1,57 @@
-#!/bin/bash
+#!/bin/sh
 #
-# this script tries to set the volume
-# 
-# requirements:
-#  on osx:   osascript
-#  on linux: aumix, awk
+# This script tries to set the volume
+# Requirements:
+# MacOSX:    osascript
+# GNU/Linux: (amixer|aumix) && awk
 #
 
-hasAumix=0
-hasOsascript=0
+for snd in amixer aumix osascript; do
+	if [ -x "`which $snd 2>/dev/null`" ]; then
+		SND=$snd
+		break
+	else
+		continue
+	fi
+done
 
-if [ -n "$(which aumix)" ]; then
-  hasAumix=1
-fi
-if [ -n "$(which osascript)" ]; then
-  hasOsascript=1
-fi
 
-if [ $hasOsascript -eq 0 -a $hasAumix -eq 0 ]; then
-  echo "please check volume.sh"
-  echo ""
-  echo "on linux, you have to install aumix and give group audio permissions to the webserver user"
-  echo ""
-  echo "on osx, you have to install osascript"
-  echo ""
-  echo "or change the volumeBin setting in config.php to a script of your own"
-  exit 1
-fi
+case $SND in
+	amixer)
+	# all devices: amixer scontrols | awk "{gsub(/'/,\"\"); print \$NF}"
+	DEV=Master
+	if [ -z "$1" ]; then
+		amixer sget "$DEV",0 | awk '/Left: Playback/ {gsub(/[\[\]\%]/,""); print $5}'
+	else
+		amixer -qc 0 set "$DEV" "$1"%
+	fi
+	;;
 
-# without arguments, return the current volume
-if [ $# == 0 ]; then
-  if [ $hasAumix -eq 1 ]; then
-    vol=`aumix -vq | awk '{ print $2 }' | tr -d ','`
-    echo $vol
-    exit
-  fi
-
-  if [ $hasOsascript -eq 1 ]; then
-    osxvol=`osascript -e "get output volume of (get volume settings)"`
-    echo $osxvol
-    exit
-  fi
-else
-  # with argument, set the current volume
-  if [ $1 -gt 0 -a $1 -lt 100 ]; then 
-    if [ $hasOsascript -eq 1 ]; then
-      `osascript -e "set volume output volume $1"`
-    fi
-    if [ $hasAumix -eq 1 ]; then
-      aumix -v $1
-    fi
-  else
-    echo "not a valid volume!"
-    exit;
-  fi
-fi
+	aumix)
+	if [ -z "$1" ]; then
+		aumix -vq | awk '{sub(/\,/,""); print $2}'
+ 	else
+		aumix -v "$1"
+	fi
+	;;
+    
+	osascript)
+	if [ -z "$1" ]; then
+		osascript -e "get output volume of (get volume settings)"
+	else
+		osascript -e "set volume output volume $1"
+	fi
+	;;
+	
+	*)
+	echo "Please check $0"
+  	echo ""
+	echo "On GNU/Linux, you have to install aumix (OSS) or amix (ALSA) and"
+	echo "give group audio permissions to the webserver user (for OSS only)."
+	echo ""
+	echo "On MacOSX, you have to install osascript (should be part of MacOSX anyway)."
+	echo ""
+	echo "Or change the volumeBin setting in config.php to a script of your own."
+	exit 1
+	;;
+esac
